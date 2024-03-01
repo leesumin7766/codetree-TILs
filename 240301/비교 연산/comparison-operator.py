fcf2bc4f71bc32f2ff4fd8e945811fe3506bc602
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-01 15:04:50 +0900

    Title: 비교 연산, Time: 115ms, Memory: 24MB, Status: Passed - CodeTree

--- 240301/비교 연산/comparison-operator.py
+++ 240301/비교 연산/comparison-operator.py
@@ -3,18 +3,18 @@ a, b = input().split()
 a = int(a)
 b = int(b)
 
-if a > b :
-    print("1")
-else :
-    print("0")
-if a > b :
-    print("1")
-else :
-    print("0")
 if a < b :
+    print("0")
+else :
+    print("1")
+if a > b :
     print("1")
 else :
     print("0")
+if a > b :
+    print("0")
+else :
+    print("1")
 if a < b :
     print("1")
 else :
