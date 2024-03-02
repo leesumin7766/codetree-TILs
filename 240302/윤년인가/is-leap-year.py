d2662c34e725f1eb89eeb45fd7354daeb5fb8174
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-02 16:39:25 +0900

    Title: 윤년인가, Time: 113ms, Memory: 24MB, Status: Passed - CodeTree

--- 240302/윤년인가/is-leap-year.py
+++ 240302/윤년인가/is-leap-year.py
@@ -1,10 +1,12 @@
 y = int(input())
 
 if y % 4 == 0 :
-    print("true")
     if y % 100 == 0 :
-        print ("true")
-    elif y % 400 == 0:
-        print("false")
+        if y % 400 == 0:
+            print ("true")
+        else:
+            print("false")
     else :
+        print("true")        
+else :
         print("false")
