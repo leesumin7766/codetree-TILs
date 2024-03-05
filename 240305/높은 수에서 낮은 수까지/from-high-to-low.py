a2cbe6daac37ef4e68daee8996197896b60bc59b
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-05 22:13:40 +0900

    Title: 높은 수에서 낮은 수까지, Time: 174ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240305/높은 수에서 낮은 수까지/from-high-to-low.py
@@ -0,0 +1,15 @@
+a , b = input().split()
+
+a = int(a)
+b = int(b)
+
+if a > b :
+    for i in range(a , b -1 , -1):
+        print(i, end = ' ')
+
+elif a < b :
+    for i in range(b , a -1 , -1) :
+        print(i , end = ' ')
+
+else :
+    print( a )
