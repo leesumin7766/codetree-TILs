6c8a8b532769d4c5acce8884be1cee178a59300e
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-06 18:26:12 +0900

    Title: 369 게임, Time: 120ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240306/369 게임/369-game.py
+++ 240306/369 게임/369-game.py
@@ -3,7 +3,7 @@ n = int(input())
 for i in range (1, n + 1) :
     if i % 3 == 0 or i -10 == 3 or i -10 == 6 or i -10 == 9  :
         print("0", end = ' ')
-    elif i == 30 < i < 40 or i == 60 < i < 70 or i == 90 < i < 100 :
+    elif 30 < i < 40 or  60 < i < 70 or 90 < i < 100 :
         print("0", end = ' ')
     else :
         print(i , end = ' ')
