522a09523c6867f27f73be9613af9fbd883058ba
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-09 10:36:21 +0900

    Title: 등장횟수 확인, Time: 112ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 240309/등장횟수 확인/check-number-of-appearances.py
@@ -0,0 +1,20 @@
+n1 = int(input())
+n2 = int(input())
+n3 = int(input())
+n4 = int(input())
+n5 = int(input())
+
+cnt = 0
+
+
+if n1 % 2 == 0 :
+        cnt += 1
+if n2 % 2 == 0 :
+        cnt += 1
+if n3 % 2 == 0 :
+        cnt += 1
+if n4 % 2 == 0 :
+        cnt += 1
+if n5 % 2 == 0 :
+        cnt += 1
+print(cnt)
