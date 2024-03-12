9898eaa0c773faa4df0a9613930f664260914f17
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-13 00:44:52 +0900

    Title: ab 사이에 있는 c, Time: 104ms, Memory: 24MB, Status: Passed - CodeTree

--- 240313/ab 사이에 있는 c/c-between-a-and-b.py
+++ 240313/ab 사이에 있는 c/c-between-a-and-b.py
@@ -4,6 +4,12 @@ a = int(a)
 b = int(b)
 c = int(c)
 
+satisfied = False
 for i in range (a , b + 1) :
     if i % c == 0 :
-        print("YES")
+        satisfied = True
+    
+if satisfied == True :
+    print("YES")
+else :
+    print("NO")
