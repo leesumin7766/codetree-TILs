3459fd410d345c4670a5e0c47be094c58daf9808
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-15 14:20:25 +0900

    Title: 소수 판별기, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240315/소수 판별기/prime-number-discriminator.py
+++ 240315/소수 판별기/prime-number-discriminator.py
@@ -2,7 +2,7 @@ n = int(input())
 
 satisfied = True
 
-if n % 2 == 0 or n % 3 == 0 or n % 5 == 0 :
+if n % 2 == 0 or n % 3 == 0 or n % 5 == 0 or n % 7 == 0 or n % 11 == 0 or n % 13 == 0:
      satisfied = False
      print("P")
 else :
