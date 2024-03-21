d1427ec3156304c8f85ad297ab496406ec96c339
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-03-22 00:04:36 +0900

    Title: 알파벳 출력하기, Time: 109ms, Memory: 24MB, Status: Passed - CodeTree

--- 240322/알파벳 출력하기/print-alphabet.py
+++ 240322/알파벳 출력하기/print-alphabet.py
@@ -6,6 +6,6 @@ for i in range(n) :
     for j in range(0,i +1) :
         print(cnt, end = "")
         cnt =chr(ord(cnt) + 1) 
-        if cnt > 'z' :
+        if cnt > 'Z' :
             cnt = 'A'
     print()
