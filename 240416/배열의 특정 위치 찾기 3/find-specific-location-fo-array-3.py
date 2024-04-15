55339416b2d37c778b35e34dfd9694324b7baa02
leesumin7766 <160687820+leesumin7766@users.noreply.github.com>
2024-04-16 00:21:47 +0900

    Title: 배열의 특정 위치 찾기 3, Time: 112ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240416/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
+++ 240416/배열의 특정 위치 찾기 3/find-specific-location-fo-array-3.py
@@ -1,5 +1,9 @@
 arr = list(map(int, input().split()))
 n = len(arr)
+cnt = 0
 for i in range (n + 1) :
+    cnt += 1
     if i == 0 :
-        print(i + arr[i -1] + arr[i-2])
+        cnt += 1
+
+        print(arr[cnt] + arr[cnt-1] + arr[cnt-2])
